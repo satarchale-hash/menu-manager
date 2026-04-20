a5a4dd3628b3e6b94997909eb6c726d91e4fd915
Tarch <satarchale@gmail.com>
2026-04-20 10:29:21 +0200

    dual entry point: index + admin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist' },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'public/admin.html'),
+      }
+    }
+  },
   publicDir: 'public',
 })
